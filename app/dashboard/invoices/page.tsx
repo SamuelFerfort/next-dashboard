1e4ed02cadfbbe2c9a6beded1383cd1586e00699
Samuel <samuferfort@gmail.com>
2024-10-08 23:10:57 +0200

    initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold">Invoices</h1>
+    </div>
+  );
+}
